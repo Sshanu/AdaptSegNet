d7edf179c6a16b110479ddd0d035d1bbd7ac07df
shanu <sshanukr@gmail.com>
2019-04-15 00:29:56 +0530

    train model without already adaptation

--- .ipynb_checkpoints/test-checkpoint.sh
+++ .ipynb_checkpoints/test-checkpoint.sh
@@ -1,2 +1,3 @@
 # CUDA_VISIBLE_DEVICES=0 python evaluate_cityscapes.py --restore-from ./snapshots/Cityscapes2IITK_multi/GTA5_10000.pth
-CUDA_VISIBLE_DEVICES=3 python evaluate_cityscapes.py --restore-from ./snapshots/GTA2IITK_multi/GTA5_10000.pth
+
+CUDA_VISIBLE_DEVICES=3 python evaluate_cityscapes.py --restore-from ./snapshots/GTA2IITK_multi/GTA5_50000.pth
