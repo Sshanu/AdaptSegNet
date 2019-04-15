55c14b17246f5abdd14a928ae493031a36a919f6
shanu <sshanukr@gmail.com>
2019-04-16 02:03:48 +0530

    city 2 iitk adapt

--- dataset/cityscapes_train_dataset.py
+++ dataset/cityscapes_train_dataset.py
@@ -8,7 +8,15 @@ import torch
 import torchvision
 from torch.utils import data
 from PIL import Image
+import json
+from os.path import join
 
+
+def label_mapping(input, mapping):
+    output = np.copy(input)
+    for ind in range(len(mapping)):
+        output[input == mapping[ind][0]] = mapping[ind][1]
+    return np.array(output, dtype=np.int64)
 
 class cityscapestrainDataSet(data.Dataset):
     def __init__(self, root, list_path, list_path_labels, max_iters=None, crop_size=(321, 321), mean=(128, 128, 128), scale=True, mirror=True, ignore_label=255, set='val'):
@@ -24,6 +32,16 @@ class cityscapestrainDataSet(data.Dataset):
         self.img_ids = [i_id.strip() for i_id in open(list_path)]
         self.label_ids = [i_id.strip() for i_id in open(list_path_labels)]
         self.set = set
+        
+        with open(join('./dataset/cityscapes_list', 'info.json'), 'r') as fp:
+            info = json.load(fp)
+        self.mapping = np.array(info['label2train'], dtype=np.int)
+        
+        self.id_to_trainid = {7: 0, 8: 1, 11: 2, 12: 3, 13: 4, 17: 5,
+                              19: 6, 20: 7, 21: 8, 22: 9, 23: 10, 24: 11, 25: 12,
+                              26: 13, 27: 14, 28: 15, 31: 16, 32: 17, 33: 18}
+
+        
         if not max_iters==None:
             self.img_ids = self.img_ids * int(np.ceil(float(max_iters) / len(self.img_ids)))
             self.label_ids = self.label_ids * int(np.ceil(float(max_iters) / len(self.label_ids)))
@@ -56,14 +74,13 @@ class cityscapestrainDataSet(data.Dataset):
 
         image = np.asarray(image, np.float32)
         label = np.asarray(label, np.float32)
-        label_copy = 255 * np.ones(label.shape, dtype=np.float32)
+        label = label_mapping(label, self.mapping)
 
         size = image.shape
         image = image[:, :, ::-1]  # change to BGR
         image -= self.mean
         image = image.transpose((2, 0, 1))
-
-        return image.copy(), label_copy.copy(), np.array(size), name
+        return image.copy(), label.copy(), np.array(size), name
 
 
 if __name__ == '__main__':
--- train_gta.sh
+++ train_gta.sh
@@ -1,3 +1,3 @@
-# CUDA_VISIBLE_DEVICES=3 python train_gta2iitk_multi.py --snapshot-dir ./snapshots/GTA2IITK_multi --lambda-seg 0.1 --restore-from ./model/GTA2Cityscapes_multi-ed35151c.pth --lambda-adv-target1 0.0002 --lambda-adv-target2 0.001
+CUDA_VISIBLE_DEVICES=3 python train_gta2iitk_multi.py --snapshot-dir ./snapshots/GTA2IITK_multi_new --lambda-seg 0.1 --restore-from ./model/GTA2Cityscapes_multi-ed35151c.pth --lambda-adv-target1 0.0002 --lambda-adv-target2 0.001
 
-CUDA_VISIBLE_DEVICES=3 python train_gta2iitk_multi_pre.py --snapshot-dir ./snapshots/GTA2IITK_multi_pre --lambda-seg 0.1 --lambda-adv-target1 0.0002 --lambda-adv-target2 0.001
+# CUDA_VISIBLE_DEVICES=3 python train_gta2iitk_multi_pre.py --snapshot-dir ./snapshots/GTA2IITK_multi_pre --lambda-seg 0.1 --lambda-adv-target1 0.0002 --lambda-adv-target2 0.001
