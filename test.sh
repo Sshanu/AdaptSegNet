4148e863ffaea542866064a9a256873571a84116
shanu <sshanukr@gmail.com>
2019-04-16 23:12:07 +0530

    log updates

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=3 python evaluate_cityscapes.py --restore-from ./snapshots/GTA2IITK_multi_pre/GTA5_50000.pth
+CUDA_VISIBLE_DEVICES=1 python evaluate_cityscapes.py --restore-from ./snapshots/gta2iitk_city_multi_pre/GTA5_55000.pth
 
 # CUDA_VISIBLE_DEVICES=3 python evaluate_cityscapes.py --restore-from ./snapshots/GTA2IITK_multi/GTA5_50000.pth
